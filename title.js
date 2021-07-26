7cedbb2914427487726659ffdd17bbe9ef35de7d
DIYDamian <33351875+DIYDamian@users.noreply.github.com>
2021-07-26 10:15:32 -0400

    Add files via upload

--- title.js
+++ title.js
@@ -45,3 +45,16 @@ var i = Math.floor(tTextA.length * Math.random());
 var j = Math.floor(tTextB.length * Math.random());
 
 document.getElementById("maintitle").innerHTML = tTextA[i] + tTextB[j];
+
+function randomRgbaString (alpha) {
+  let r = Math.floor(Math.random() * 255)
+  let g = Math.floor(Math.random() * 255)
+  let b = Math.floor(Math.random() * 255)
+  let a = alpha
+  return `rgba(${r},${g},${b},${a})`
+}
+
+setInterval(function(){
+	var myBox  = document.getElementById("fullShit");
+	myBox.style.outlineColor = randomRgbaString(.9);
+}, 42);
