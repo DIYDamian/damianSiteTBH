69236ab4a985ca93bea6b24e3e38446ed1094a87
DIYDamian <33351875+DIYDamian@users.noreply.github.com>
2021-07-26 14:43:27 -0400

    Add files via upload

--- uej.js
+++ uej.js
@@ -1,4 +1,5 @@
 let speed = 20;
+let grav = (Math.random() * Math.PI * 2.0);
 let scale = 0.17; // Image scale (I work on 1080p monitor)
 let canvas;
 let ctx;
@@ -12,6 +13,10 @@ let dvd = {
     img: new Image()
 };
 
+const clamp = (num, min, max) => {
+	Math.min(Math.max(num, min), max);
+}
+
 (function main(){
     canvas = document.getElementById("thereHeGo");
     ctx = canvas.getContext("2d");
@@ -20,6 +25,9 @@ let dvd = {
     //Draw the "tv screen"
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
+	
+	dvd.x = (canvas.width-(dvd.img.width*scale*2))*0.5;
+	dvd.y = (canvas.height-(dvd.img.height*scale*2))*0.5;
 
     pickColor();
     update();
@@ -31,11 +39,13 @@ function update() {
         ctx.clearRect (0, 0, canvas.width, canvas.height);
         //Draw DVD Logo and his background
         ctx.fillStyle = logoColor;
-        ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        ctx.fillRect(dvd.x-2, dvd.y-2, (dvd.img.width*scale)+4, (dvd.img.height*scale)+4);
         ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
         //Move the logo
         dvd.x+=dvd.xspeed;
         dvd.y+=dvd.yspeed;
+		dvd.xspeed-=Math.sin(grav)*0.1;
+		dvd.yspeed-=Math.cos(grav)*0.1;
         //Check for collision 
         checkHitBox();
         update();   
@@ -44,16 +54,30 @@ function update() {
 
 //Check for border collision
 function checkHitBox(){
-    if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
-        dvd.xspeed *= -1;
-        pickColor();
+    if((dvd.x+dvd.img.width*scale >= canvas.width && dvd.xspeed > 0) || (dvd.x <= 0 && dvd.xspeed < 0)){
+        dvd.xspeed *= -.31;
+        dvd.yspeed *= .31;
+		let vel = Math.abs(dvd.xspeed);
+		console.log("X - " + vel);
+		if(vel >=.1){
+			pickColor();
+		}
     }
         
-    if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
-        dvd.yspeed *= -1;
-        pickColor();
+    if((dvd.y+dvd.img.height*scale >= canvas.height && dvd.yspeed > 0) || (dvd.y <= 0 && dvd.yspeed < 0)){
+        dvd.yspeed *= -.31;
+        dvd.xspeed *= .31;
+		let vel = Math.abs(dvd.yspeed);
+		console.log("Y - " + vel);
+		if(vel >=.1){
+			pickColor();
+		}
     }    
 }
+
+setInterval(function(){
+	grav = (Math.random() * Math.PI * 2.0);
+}, 5000)
 
 //Pick a random color in RGB format
 function pickColor(){
